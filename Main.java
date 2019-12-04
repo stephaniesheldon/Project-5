2eb5a90a11aca20cf850a2a7d14c06fb17a5704c
Stephanie Sheldon <S.Camille.Sheldon>
2019-12-03 19:35:10 -0600

    grid pane set up

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
@@ -18,17 +20,33 @@ public class Main extends Application {
 		try {
 			primaryStage.setTitle("Hamming Distance");
 			
+			
+			//create grid pane
+			GridPane grid = new GridPane();
+			grid.setHgap(10);
+			grid.setVgap(10);
+			grid.setPadding(new Insets(0,10,0,10));
+			
+			
 			//initialize buttons
 			showStation = new Button("Show Station");
 			calcHD = new Button("Calculate HD");
 			addStation = new Button("Add Station");
 			
 			
+			grid.add(calcHD,0,0);
+			grid.add(addStation,1,1,2,2);
+			
+			Scene scene = new Scene(grid,400,800);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			
+			/*
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			*/
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
